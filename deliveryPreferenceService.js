35fe02dfd023b2723133160fb7a97970ee5439e5
pishike khyathi <102966097+pishikekhyathi94@users.noreply.github.com>
2025-04-29 18:51:36 -0500

    Delete deliveryPreferenceService.js

--- deliveryPreferenceService.js
+++
@@ -1,5 +0,0 @@
-function updateDeliveryPreference(accountNumber, preference) {
-  if (!accountNumber || !preference) throw new Error('Invalid data');
-  return { status: 'success', accountNumber, preference };
-}
-module.exports = { updateDeliveryPreference };
